f91f81159d0f33c05dff65a8d60ab2afd440f2b0
BenKoehler <ben.koehler@web.de>
2022-10-16 10:45:50 +0200

    replace CMAKE_CURRENT_SOURCE_DIR with PROJECT_SOURCE_DIR

--- bk.cmake
+++ bk.cmake
@@ -1,7 +1,7 @@
 add_library(date INTERFACE EXCLUDE_FROM_ALL)
 target_compile_features(date INTERFACE cxx_std_17)
 target_compile_definitions(date INTERFACE LIBRARY_HEADER_ONLY)
-target_include_directories(date SYSTEM INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/date/include)
+target_include_directories(date SYSTEM INTERFACE ${PROJECT_SOURCE_DIR}/thirdparty/date/include)
 
 function(link_date target MOD)
     target_link_libraries(${target} ${MOD} date)
